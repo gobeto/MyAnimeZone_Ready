6e776d48c067258f89afbc0d786c19468b080337
gobeto <georgi_vacelov@abv.bg>
2024-03-14 18:27:41 +0200

    yfyu

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -10,25 +10,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const { email, name, password } = req.body;
 
-    let existingUser;
-
+    const existingUser = await prismadb.user.findUnique({
+      where: {
+        email
+      }
+    })
     try{
-    try {
-      existingUser = await prismadb.user.findUnique({
-        where: {
-          email
-        }
-      });
-    } catch (error) {
-      return res.status(400).json({ error: `Something went up wrong: ${error}` });
-    }
-
     if (existingUser) {
-      return res.status(422).json({ error: 'Email taken' });
+      //return res.status(422).json({ error: 'Email taken' });
     }
-  } catch (error) {
-    return res.status(400).json({ error: `Something went down wrong: ${error}` });
-  }
+  } catch (error) { return res.status(400).json({ error: `Something went wrong: ${error}` });}
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
