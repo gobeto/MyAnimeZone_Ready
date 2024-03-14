21cc53257f56422988070e337eeab238ae39db47
gobeto <georgi_vacelov@abv.bg>
2024-03-14 18:03:44 +0200

    reg

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,72 +1,39 @@
-import bcrypt from "bcrypt";
-import { NextApiRequest, NextApiResponse } from "next";
-import prismadb from "@/lib/prismadb";
+import bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/lib/prismadb';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method != "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
+    if (req.method !== 'POST') {
+      return res.status(405).end();
+    }
+
     const { email, name, password } = req.body;
 
-    if (!email || !name || !password) {
-      return res.status(400).json({ error: "Missing required fields" });
+    const existingUser = await prismadb.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    if (existingUser) {
+      return res.status(422).json({ error: 'Email taken' });
     }
 
-    // const existingUser = await prismadb.user.findUnique({
-    //   where: {
-    //     email,
-    //   },
-    // });
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-    // if (existingUser) {
-    //   return res.status(422).json({ error: "Email taken" });
-    // }
-
-    // const hashedPassword = await bcrypt.hash(password, 12);
-
-    // const user = await prismadb.user.create({
-    //   data: {
-    //     email,
-    //     name,
-    //     hashedPassword,
-    //     emailVerified: new Date(),
-    //     isAdmin: false,
-    //   },
-    // });
-    // return res.status(200).json(user);
-    
-    let hashedPassword;
-    try {
-      hashedPassword = await bcrypt.hash(password, 12);
-    } catch (error) {
-      console.error("Error hashing password:", error);
-      return res.status(500).json({ error: "Error hashing password" });
-    }
-
-    let user;
-    try {
-      user = await prismadb.user.create({
-        data: {
-          email,
-          name,
-          hashedPassword,
-          emailVerified: new Date(),
-          isAdmin: false,
-        },
-      });
-    } catch (error) {
-      console.error("Error creating user:", error);
-      return res.status(500).json({ error: "Error creating user" });
-    }
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: '',
+        emailVerified: new Date(),
+      }
+    })
 
     return res.status(200).json(user);
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: "Internal server error" });
+    return res.status(400).json({ error: `Something went wrong: ${error}` });
   }
 }
