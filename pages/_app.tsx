9cb994141199d3d9de101c5e1b902bf51c274aae
gobeto <georgi_vacelov@abv.bg>
2024-03-25 16:55:13 +0200

    hope

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,39 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from "../i18n"; // path to your i18n config
 
 
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+
+//check if available session exist and if it doesnt it redirect to /auth
+export async function getServerSideProps(context: NextPageContext) {
+  try {
+    const session = await getSession(context);
+
+    console.log('Session:', session);
+    console.log('Context:', context);
+
+    if (!session) {
+      console.log('No session found, redirecting to /auth');
+      return {
+        redirect: {
+          destination: "/auth",
+          permanent: false,
+        },
+      };
+    }
+    return {
+      props: {},
+    };
+  } catch (error) {
+    console.error('Error getting session:', error);
+    return {
+      redirect: {
+        destination: "/error",
+        permanent: false,
+      },
+    };
+  }
+}
 
 export default function App({ 
   Component, 
