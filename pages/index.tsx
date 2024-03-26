e4af506591f432fe3989269096690979f84e1fde
gobeto <georgi_vacelov@abv.bg>
2024-03-26 16:47:31 +0200

    hope

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export async function getServerSideProps(context: NextPageContext) {
     console.log('No session found, redirecting to /auth');
     return {
       redirect: {
-        destination: "/auth",
+        destination: "/library",
         permanent: false,
       },
     };
