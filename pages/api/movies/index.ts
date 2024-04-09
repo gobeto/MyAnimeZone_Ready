335c6d6d1801dd39f1642f4ebf1f0ea5df3fc60a
gobeto <georgi_vacelov@abv.bg>
2024-04-09 14:36:00 +0300

    bj

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -13,7 +13,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 
         const movies = await prismadb.movie.findMany()
 
-        return res.status(400).json(movies);
+        return res.status(200).json(movies);
     }catch(error){
         console.log(error);
         return res.status(400).end();
