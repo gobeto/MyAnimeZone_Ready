edd648ccc9c01dbe4e826a2889a0b3428600bcc8
gobeto <georgi_vacelov@abv.bg>
2024-04-16 14:44:06 +0300

    hhhh

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -41,7 +41,8 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
         </div>
         <hr className="bg-gray-600 border-0 h-px my-4" />
         <div
-          onClick={() => signOut({ callbackUrl: '/auth' })}
+          onClick={() => signOut()}
+          //onClick={() => signOut({ callbackUrl: '/auth' })}
           className="px-3 text-center text-white text-sm hover:underline"
         >
           {t("Sign out")}
