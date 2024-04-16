b6137b825edded5bf892a592df551716850c8487
gobeto <georgi_vacelov@abv.bg>
2024-04-16 16:24:11 +0300

    if-state

--- pages/anime/[movieId].tsx
+++ pages/anime/[movieId].tsx
@@ -27,15 +27,15 @@ export async function getServerSideProps(context: NextPageContext) {
     return null;
   });
 
-  // if (!session) {
-  //   console.log('No session found, redirecting to /auth');
-  //   return {
-  //     redirect: {
-  //       destination: "/auth",
-  //       permanent: false,
-  //     },
-  //   };
-  // }
+  if (!session) {
+    console.log('No session found, redirecting to /auth');
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
 
   return { props: {} };
 }
